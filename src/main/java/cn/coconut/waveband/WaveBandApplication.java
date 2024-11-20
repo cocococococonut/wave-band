a8d28c65dcd99a879d14479c26a498340f6b0c41
coconut <coconut3980@gmail.com>
2024-11-20 20:55:42 +0800

    Initial commit

---
+++ src/main/java/cn/coconut/waveband/WaveBandApplication.java
@@ -0,0 +1,13 @@
+package cn.coconut.waveband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveBandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaveBandApplication.class, args);
+    }
+
+}
